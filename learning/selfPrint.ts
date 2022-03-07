ee9c998bbaad03528e33caaf0145734ca03f4d99
jweken <jweken@kpnmail.nl>
2022-03-07 08:45:43 +0100

    fix imports

--- learning/selfPrint.ts
+++ learning/selfPrint.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line: typedef
 
-export function selfPrint(text: NonNullable) {
+export function selfPrint(text: string) {
     console.log("This function will automatically print this statement");
 }
-selfPrint('aaaa')';
+selfPrint('aaaa');
